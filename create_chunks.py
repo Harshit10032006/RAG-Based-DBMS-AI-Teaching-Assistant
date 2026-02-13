a60ce5fcf42c2508f8963e1e4f027f62e396e399
Harshit <harshitkholiya1003@gmail.com>
2026-02-13 22:29:42 +0530

    Created Chunks

---
+++ create_chunks.py
@@ -0,0 +1,29 @@
+import os 
+import whisper
+import os 
+import json
+
+model=whisper.load_model('large')
+
+audios=os.listdir('Audio(SQL)')
+
+
+for audio in audios :
+    number=audio.split('：')[0].split('Lecture')[1]
+    Title=audio.split('：')[1].split('.mp3')[0]
+    
+    result=model.transcribe(audio=f'Audio(SQL)/{audio}',language='hi',task='translate')
+    
+
+
+
+
+    chunks=[]
+
+    for segment in result['segments']:
+        chunks.append({'number':number,'title':Title,'start':segment['start'],'end':segment['end'],'text':segment['text']})
+
+    chunks_withdata={'chunks':chunks,'text':result['text']}
+    
+    with open (f'jsons/{audio}.json','w') as f :
+     json.dump(chunks_withdata,f)
